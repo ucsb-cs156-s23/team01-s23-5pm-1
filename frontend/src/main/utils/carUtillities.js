fe6e6b2fa93aca1e9199a6be7180596fd0f1a21d
TG6S85 <joshuageorget@gmail.com>
2023-04-25 15:17:27 -0700

    "jt - updated carUtils"

--- frontend/src/main/utils/carUtillities.js
+++ frontend/src/main/utils/carUtillities.js
@@ -1,13 +1,12 @@
-// get cars from local storage
 const get = () => {
-    const carValue = localStorage.getItem("car");
+    const carValue = localStorage.getItem("cars");
     if (carValue === undefined) {
-        const carCollection = { nextId: 1, cars: [] }
+        const carCollection = { nextId: 1, cars: [] };
         return set(carCollection);
     }
     const carCollection = JSON.parse(carValue);
     if (carCollection === null) {
-        const carCollection = { nextId: 1, cars: [] }
+        const carCollection = { nextId: 1, cars: [] };
         return set(carCollection);
     }
     return carCollection;
@@ -20,21 +19,19 @@ const getById = (id) => {
     const carCollection = get();
     const cars = carCollection.cars;
 
-    /* eslint-disable-next-line eqeqeq */ // we really do want == here, not ===
+    /* eslint-disable-next-line eqeqeq */
     const index = cars.findIndex((r) => r.id == id);
     if (index === -1) {
-        return { "error": `car with id ${id} not found` };
+        return { "error": `car with id ${id} not found`};
     }
     return { car: cars[index] };
-}
+};
 
-// set cars in local storage
 const set = (carCollection) => {
     localStorage.setItem("cars", JSON.stringify(carCollection));
     return carCollection;
 };
 
-// add a car to local storage
 const add = (car) => {
     const carCollection = get();
     car = { ...car, id: carCollection.nextId };
@@ -44,23 +41,20 @@ const add = (car) => {
     return car;
 };
 
-// update a car in local storage
 const update = (car) => {
     const carCollection = get();
-
     const cars = carCollection.cars;
 
-    /* eslint-disable-next-line eqeqeq */ // we really do want == here, not ===
+    /* eslint-disable-next-line eqeqeq */
     const index = cars.findIndex((r) => r.id == car.id);
     if (index === -1) {
         return { "error": `car with id ${car.id} not found` };
     }
     cars[index] = car;
     set(carCollection);
-    return { carCollection: carCollection };
+    return { carCollection: carCollection};
 };
 
-// delete a car from local storage
 const del = (id) => {
     if (id === undefined) {
         return { "error": "id is a required parameter" };
@@ -68,7 +62,7 @@ const del = (id) => {
     const carCollection = get();
     const cars = carCollection.cars;
 
-    /* eslint-disable-next-line eqeqeq */ // we really do want == here, not ===
+    /* eslint-disable-next-line eqeqeq */
     const index = cars.findIndex((r) => r.id == id);
     if (index === -1) {
         return { "error": `car with id ${id} not found` };
@@ -78,7 +72,7 @@ const del = (id) => {
     return { carCollection: carCollection };
 };
 
-const carUtils = {
+const carUtilities = {
     get,
     getById,
     add,
@@ -86,4 +80,4 @@ const carUtils = {
     del
 };
 
-export { carUtils };
+export { carUtilities };
