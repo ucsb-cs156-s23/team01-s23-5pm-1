021ce33e23cc566c4b34d07a73965a50f9b151a4
cmfisher606 <cmfisher606@gmail.com>
2023-04-25 11:56:13 -0700

    cf - Added a form for books

---
+++ frontend/src/main/components/Books/BookForm.js
@@ -0,0 +1,147 @@
+import React from 'react'
+import { Button, Form } from 'react-bootstrap';
+import { useForm } from 'react-hook-form'
+import { useNavigate } from 'react-router-dom';
+
+function BookForm({ initialContents, submitAction, buttonLabel = "Create" }) {
+
+    const navigate = useNavigate();
+    
+    // Stryker disable all
+    const {
+        register,
+        formState: { errors },
+        handleSubmit,
+    } = useForm(
+        { defaultValues: initialContents || {}, }
+    );
+    // Stryker enable all
+   
+    const testIdPrefix = "BookForm";
+
+    return (
+
+        <Form onSubmit={handleSubmit(submitAction)}>
+
+            {initialContents && (
+                <Form.Group className="mb-3" >
+                    <Form.Label htmlFor="id">Id</Form.Label>
+                    <Form.Control
+                        data-testid={testIdPrefix + "-id"}
+                        id="id"
+                        type="text"
+                        {...register("id")}
+                        value={initialContents.id}
+                        disabled
+                    />
+                </Form.Group>
+            )}
+            
+            {/* The three Form.Group's below are for Book */}
+            <Form.Group className="mb-3" >
+                <Form.Label htmlFor="title">Title</Form.Label>
+                <Form.Control
+                    data-testid={testIdPrefix + "-title"}
+                    id="title"
+                    type="text"
+                    isInvalid={Boolean(errors.title)}
+                    {...register("title", {
+                        required: "Title is required."
+                    })}
+                />
+                <Form.Control.Feedback type="invalid">
+                    {errors.title?.message}
+                </Form.Control.Feedback>
+            </Form.Group>  
+
+            <Form.Group className="mb-3" >
+                <Form.Label htmlFor="author">Author</Form.Label>
+                <Form.Control
+                    data-testid={testIdPrefix + "-author"}
+                    id="author"
+                    type="text"
+                    isInvalid={Boolean(errors.author)}
+                    {...register("author", {
+                        required: "author is required."
+                    })}
+                />
+                <Form.Control.Feedback type="invalid">
+                    {errors.author?.message}
+                </Form.Control.Feedback>
+            </Form.Group>
+
+            <Form.Group className="mb-3" >
+                <Form.Label htmlFor="year">Year</Form.Label>
+                <Form.Control
+                    data-testid={testIdPrefix + "-year"}
+                    id="year"
+                    type="text"
+                    isInvalid={Boolean(errors.year)}
+                    {...register("year", {
+                        required: "Year is required."
+                    })}
+                />
+                <Form.Control.Feedback type="invalid">
+                    {errors.year?.message}
+                </Form.Control.Feedback>
+            </Form.Group>                 
+
+            {/* The two Form.Group's below were for Restaurant which is why they are commented out */}
+
+            {/* <Form.Group className="mb-3" >
+                <Form.Label htmlFor="name">Name</Form.Label>
+                <Form.Control
+                    data-testid={testIdPrefix + "-name"}
+                    id="name"
+                    type="text"
+                    isInvalid={Boolean(errors.name)}
+                    {...register("name", {
+                        required: "Name is required.",
+                        maxLength : {
+                            value: 30,
+                            message: "Max length 30 characters"
+                        }
+                    })}
+                />
+                <Form.Control.Feedback type="invalid">
+                    {errors.name?.message}
+                </Form.Control.Feedback>
+            </Form.Group>
+
+            <Form.Group className="mb-3" >
+                <Form.Label htmlFor="description">Description</Form.Label>
+                <Form.Control
+                    data-testid={testIdPrefix + "-description"}
+                    id="description"
+                    type="text"
+                    isInvalid={Boolean(errors.description)}
+                    {...register("description", {
+                        required: "Description is required."
+                    })}
+                />
+                <Form.Control.Feedback type="invalid">
+                    {errors.description?.message}
+                </Form.Control.Feedback>
+            </Form.Group> */}
+
+
+            <Button
+                type="submit"
+                data-testid={testIdPrefix + "-submit"}
+            >
+                {buttonLabel}
+            </Button>
+            <Button
+                variant="Secondary"
+                onClick={() => navigate(-1)}
+                data-testid={testIdPrefix + "-cancel"}
+            >
+                Cancel
+            </Button>
+
+        </Form>
+
+    )
+}
+
+export default BookForm;
