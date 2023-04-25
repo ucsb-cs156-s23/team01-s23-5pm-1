e2ca0518bd35e652f9615bae337df9dbef40ea45
cmfisher606 <cmfisher606@gmail.com>
2023-04-25 15:15:12 -0700

    cf - Added BookEditPage.test file

---
+++ frontend/src/tests/pages/Books/BookEditPage.test.js
@@ -0,0 +1,124 @@
+import { render, screen, act, waitFor, fireEvent } from "@testing-library/react";
+import BookEditPage from "main/pages/Books/BookEditPage";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { MemoryRouter } from "react-router-dom";
+import mockConsole from "jest-mock-console";
+
+const mockNavigate = jest.fn();
+
+jest.mock('react-router-dom', () => ({
+    ...jest.requireActual('react-router-dom'),
+    useParams: () => ({
+        id: 3
+    }),
+    useNavigate: () => mockNavigate
+}));
+
+const mockUpdate = jest.fn();
+jest.mock('main/utils/bookUtils', () => {
+    return {
+        __esModule: true,
+        bookUtils: {
+            update: (_book) => {return mockUpdate();},
+            getById: (_id) => {
+                return {
+                    book: {
+                        id: 3,
+                        title: "Harry Potter",
+                        author: "J. K. Rowling",
+                        year: "1997"
+                    }
+                }
+            }
+        }
+    }
+});
+
+
+describe("BookEditPage tests", () => {
+
+    const queryClient = new QueryClient();
+
+    test("renders without crashing", () => {
+        render(
+            <QueryClientProvider client={queryClient}>
+                <MemoryRouter>
+                    <BookEditPage />
+                </MemoryRouter>
+            </QueryClientProvider>
+        );
+    });
+
+    test("loads the correct fields", async () => {
+
+        render(
+            <QueryClientProvider client={queryClient}>
+                <MemoryRouter>
+                    <BookEditPage />
+                </MemoryRouter>
+            </QueryClientProvider>
+        );
+
+        expect(screen.getByTestId("BookForm-name")).toBeInTheDocument();
+        expect(screen.getByDisplayValue('Harry Potter')).toBeInTheDocument();
+        expect(screen.getByDisplayValue('J. K. Rowling')).toBeInTheDocument();
+        expect(screen.getByDisplayValue('1997')).toBeInTheDocument();
+    });
+
+    test("redirects to /books on submit", async () => {
+
+        const restoreConsole = mockConsole();
+
+        mockUpdate.mockReturnValue({
+            "book": {
+                id: 3,
+                title: "Harry Potter",
+                author: "J. K. Rowling",
+                year: "1997"
+            }
+        });
+
+        render(
+            <QueryClientProvider client={queryClient}>
+                <MemoryRouter>
+                    <BookEditPage />
+                </MemoryRouter>
+            </QueryClientProvider>
+        )
+
+        const titleInput = screen.getByLabelText("Title");
+        expect(titleInput).toBeInTheDocument();
+
+
+        const authorInput = screen.getByLabelText("Author");
+        expect(authorInput).toBeInTheDocument();
+
+        const yearInput = screen.getByLabelText("Year");
+        expect(yearInput).toBeInTheDocument();
+
+        const updateButton = screen.getByText("Update");
+        expect(updateButton).toBeInTheDocument();
+
+        await act(async () => {
+            fireEvent.change(titleInput, { target: { value: 'Harry Potter' } })
+            fireEvent.change(authorInput, { target: { value: 'J. K. Rowling' } })
+            fireEvent.change(yearInput, { target: { value: '1997' } })
+            fireEvent.click(updateButton);
+        });
+
+        await waitFor(() => expect(mockUpdate).toHaveBeenCalled());
+        await waitFor(() => expect(mockNavigate).toHaveBeenCalledWith("/books"));
+
+        // assert - check that the console.log was called with the expected message
+        expect(console.log).toHaveBeenCalled();
+        const message = console.log.mock.calls[0][0];
+        const expectedMessage =  `updatedBook: {"book":{"id":3,"title":"Harry Potter","author":"J. K. Rowling","year":"1997"}`
+
+        expect(message).toMatch(expectedMessage);
+        restoreConsole();
+
+    });
+
+});
+
+
